8ff892d1621fd4ce6e5808e5c5af7cb56504041c
Clement Creusat <clement.creusat@gmail.com>
2024-11-25 13:30:53 +0100

    fix: fix baseUrl storybook and apps

--- packages/react/ui/src/hooks/useImage/useImage.stories.tsx
+++ packages/react/ui/src/hooks/useImage/useImage.stories.tsx
@@ -8,12 +8,13 @@ const meta: Meta<typeof useImage> = {
 export default meta;
 type Story = StoryObj<typeof useImage>;
 
+const base = import.meta.env.BASE_URL;
+
 export const Example: Story = {
   render: (args) => {
     const src = "";
     const alt = "alternative text";
-    const placeholder =
-      "/assets/themes/edifice-bootstrap/images/common/image-placeholder.png";
+    const placeholder = `${base}assets/themes/edifice-bootstrap/images/common/image-placeholder.png`;
     const { imgSrc, onError } = useImage({ src, placeholder });
 
     return <img alt={alt} onError={onError} src={imgSrc} />;
