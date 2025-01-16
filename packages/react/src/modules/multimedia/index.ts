e9236359ff1dd7a293bae7ee8fe15c8b1676eef9
Clement Creusat <clement.creusat@gmail.com>
2025-01-16 10:22:20 +0100

    fix: ENABLING-188, add imagepicker export

--- packages/react/src/modules/multimedia/index.ts
+++ packages/react/src/modules/multimedia/index.ts
@@ -1,6 +1,7 @@
 export * from './AudioRecorder';
 export * from './Embed';
 export * from './ImageEditor';
+export * from './ImagePicker';
 export * from './Linker';
 export * from './MediaLibrary';
 export * from './VideoRecorder';
