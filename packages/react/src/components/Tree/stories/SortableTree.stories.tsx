23eadc8685c1c8fe3f4aef0088769e20cdb5af3e
Clément Creusat <clement.creusat@gmail.com>
2025-01-20 12:14:37 +0100

    docs: ENABLING-198, update docs Tree & SortableTree (#312)

--- packages/react/src/components/Tree/stories/SortableTree.stories.tsx
+++ packages/react/src/components/Tree/stories/SortableTree.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import SortableTree from '../components/SortableTree';
-import { SortableTreeProps } from '../types';
+import { SortableTreeProps, UpdateTreeData } from '../types';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof SortableTree> = {
@@ -72,6 +72,178 @@ export const Base: Story = {
       description: {
         story:
           'SortableTree component allows to drag and drop tree nodes to reorder them',
+      },
+    },
+  },
+};
+
+export const WithIcons: Story = {
+  render: (args) => <SortableTree {...args} showIcon />,
+  args: {
+    ...meta.args,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'SortableTree component with icons displayed for each node.',
+      },
+      source: {
+        code: `<SortableTree showIcon />`,
+      },
+    },
+  },
+};
+
+export const ExpandedNodes: Story = {
+  render: (args) => <SortableTree {...args} shouldExpandAllNodes />,
+  args: {
+    ...meta.args,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'SortableTree component with all nodes expanded by default.',
+      },
+      source: {
+        code: `<SortableTree shouldExpandAllNodes />`,
+      },
+    },
+  },
+};
+
+export const DisabledNodes: Story = {
+  render: (args) => (
+    <SortableTree
+      {...args}
+      isDisabled={(node) => node === '3' || node === '6'}
+    />
+  ),
+  args: {
+    ...meta.args,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'SortableTree component with some nodes disabled. These nodes cannot be dragged or dropped.',
+      },
+      source: {
+        code: ` <SortableTree isDisabled={(node) => node === '3' || node === '6'} />`,
+      },
+    },
+  },
+};
+
+export const DisabledAllNodes: Story = {
+  render: (args) => <SortableTree {...args} isDisabled={() => true} />,
+  args: {
+    ...meta.args,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'SortableTree component with all nodes disabled. No nodes can be dragged or dropped.',
+      },
+      source: {
+        code: `<SortableTree isDisabled={() => true} />`,
+      },
+    },
+  },
+};
+
+export const CustomRenderNode: Story = {
+  render: (args) => (
+    <SortableTree
+      {...args}
+      renderNode={(payload) => (
+        <div className="custom-node">
+          <span>{payload?.node?.name}</span> - <span>{payload?.node?.id}</span>
+        </div>
+      )}
+    />
+  ),
+  args: {
+    ...meta.args,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'SortableTree component with a custom renderNode function.',
+      },
+      source: {
+        code: `<SortableTree
+  renderNode={(payload) => (
+    <div className="custom-node">
+      <span>{payload?.node?.name}</span> - <span>{payload?.node?.id}</span>
+    </div>
+  )}
+/>`,
+      },
+    },
+  },
+};
+
+const fakePUTRequest = (nodes: UpdateTreeData[]) => {
+  console.log('Fake API call completed');
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          pages: nodes.map((page) => ({
+            _id: page._id,
+            parentId: page.parentId!,
+            position: page.position ?? 0,
+          })),
+        },
+      });
+    }, 1000);
+  });
+};
+
+export const UpdatePagesOnSortable: Story = {
+  render: (args) => (
+    <SortableTree
+      {...args}
+      onSortable={async (nodes) => {
+        const updatedPosition = await fakePUTRequest(nodes);
+        alert(JSON.stringify(updatedPosition));
+      }}
+    />
+  ),
+  args: {
+    ...meta.args,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'SortableTree component with onSortable prop to update the position of pages via an API PUT request.',
+      },
+      source: {
+        code: `<SortableTree onSortable={async (nodes) => {
+  const fakePUTRequest = (nodes: UpdateTreeData[]) => {
+  console.log('Fake API call completed');
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          pages: nodes.map((page) => ({
+            _id: page._id,
+            parentId: page.parentId!,
+            position: page.position ?? 0,
+          })),
+        },
+      });
+    }, 1000);
+  });
+};
+
+  const updatedPosition = await fakePUTRequest(nodes);
+  alert(JSON.stringify(updatedPosition));
+}}
+/>`,
       },
     },
   },
