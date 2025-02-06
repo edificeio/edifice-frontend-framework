950bdbdba5d0f572e0f2289909c0b920000b0736
RomuC <romuald.coulaud@gmail.com>
2025-02-06 14:34:27 +0100

    add missing export

--- packages/react/src/modules/editor/components/Renderer/index.ts
+++ packages/react/src/modules/editor/components/Renderer/index.ts
@@ -1,4 +1,5 @@
-export { default as AudioRenderer } from './AudioRenderer';
 export { default as AttachmentRenderer } from './AttachmentRenderer';
+export { default as AudioRenderer } from './AudioRenderer';
+export { default as ConversationHistoryRenderer } from './ConversationHistoryRenderer';
 export { default as LinkerRenderer } from './LinkerRenderer';
 export { default as MediaRenderer } from './MediaRenderer';
