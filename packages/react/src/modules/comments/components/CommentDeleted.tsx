dba99ef16274bdbecbe0cf7f36e78006186c9e4e
david-cc <david.caudeli@edifice.io>
2025-03-18 16:34:23 +0100

    feat(comments): #WB2-1941, show Deleted Comments (#334)

---
+++ packages/react/src/modules/comments/components/CommentDeleted.tsx
@@ -0,0 +1,11 @@
+import { useTranslation } from 'react-i18next';
+
+export const CommentDeleted = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="border rounded-3 p-12 pb-8 d-flex gap-12 bg-gray-200 my-16">
+      {t('comment.deleted')}
+    </div>
+  );
+};
