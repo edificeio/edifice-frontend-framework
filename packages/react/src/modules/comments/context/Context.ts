1b090c65876d5eee3f79030a095223c019204a8e
david-cc <david.caudeli@web-education.net>
2025-03-19 19:17:33 +0100

    fix: #WB2-2627, fix reply to comment delete issues

--- packages/react/src/modules/comments/context/Context.ts
+++ packages/react/src/modules/comments/context/Context.ts
@@ -10,14 +10,17 @@ import {
 export const CommentContext = createContext<{
   comments: CommentProps[] | undefined;
   editCommentId: string | null;
+  replyToCommentId: string | null;
   profiles: (UserProfileResult | undefined)[];
   options: Partial<CommentOptions>;
   type: CommentType;
   userRights?: Record<RightRole, boolean>;
   setEditCommentId: (value: string | null) => void;
+  setReplyToCommentId: (value: string | null) => void;
   handleModifyComment: (commentId: string) => void;
   handleCreateComment: (content: string, replyTo?: string) => void;
   handleUpdateComment: (comment: string) => void;
   handleDeleteComment: (id: string) => void;
+  handleReplyToComment: (commentId: string) => void;
   handleReset: () => void;
 } | null>(null);
