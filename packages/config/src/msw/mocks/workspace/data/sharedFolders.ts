a5c976491c96b0bc1eda903379ce5dcaf107e445
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-04-22 10:27:09 +0200

    feat(WorkspaceFolders):WB-3971 show folder without contrib right to browse into it (#356)

--- packages/config/src/msw/mocks/workspace/data/sharedFolders.ts
+++ packages/config/src/msw/mocks/workspace/data/sharedFolders.ts
@@ -4,6 +4,7 @@ export const folderOfUser = {
   _id: 'cdfc982f-421a-462f-9045-90f292bb3f33',
   owner: userInfo.userId,
   name: 'Images',
+  isShared: true,
 };
 
 export const subFolderOfUser = {
@@ -11,6 +12,7 @@ export const subFolderOfUser = {
   owner: userInfo.userId,
   name: 'Sub Images',
   eParent: 'cdfc982f-421a-462f-9045-90f292bb3f33',
+  isShared: true,
 };
 
 export const folderOfOtherWithoutContribRights = {
@@ -18,6 +20,23 @@ export const folderOfOtherWithoutContribRights = {
   owner: '1493a7fb-fb90-4acb-a1d8-7c579eba7583',
   ownerName: 'Jeanne Martin',
   name: 'Périscolaire',
+  isShared: true,
+};
+
+export const subFolderOfOtherWithUserContribRights = {
+  _id: 'bdab9120-2142-447a-94b0-382a34fecc06',
+  name: 'ParaScolaire',
+  inheritedShares: [
+    {
+      'userId': userInfo.userId,
+      'org-entcore-workspace-controllers-WorkspaceController|updateDocument':
+        true,
+    },
+  ],
+  owner: '1493a7fb-fb90-4acb-a1d8-7c579eba7583',
+  ownerName: 'Jeanne Martin',
+  eParent: '0d9ec0ee-97b5-4d33-b636-a4ca2ad1d332',
+  isShared: true,
 };
 
 export const folderOfOtherWithUserContribRights = {
@@ -31,6 +50,7 @@ export const folderOfOtherWithUserContribRights = {
     },
   ],
   owner: '971d1c4a-b784-4d0e-84d3-77efb70c4479',
+  isShared: true,
 };
 
 export const folderOfOtherWithGroupContribRights = {
@@ -44,12 +64,14 @@ export const folderOfOtherWithGroupContribRights = {
     },
   ],
   owner: 'b92e3d37-16b0-4ed9-b4c3-992091687132',
+  isShared: true,
 };
 
 export const sharedFolders = [
   folderOfUser,
   subFolderOfUser,
   folderOfOtherWithoutContribRights,
+  subFolderOfOtherWithUserContribRights,
   folderOfOtherWithUserContribRights,
   folderOfOtherWithGroupContribRights,
 ];
