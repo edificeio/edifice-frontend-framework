9764417bfb18ffbd71d0575bbc77f8e4cc0a83c3
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2025-05-07 17:10:07 +0200

    feat: #WB2-2565, add scroll in table component

--- packages/react/src/components/Table/components/Table.tsx
+++ packages/react/src/components/Table/components/Table.tsx
@@ -3,7 +3,7 @@
  * @see WAI-ARIA https://www.w3.org/WAI/ARIA/apg/patterns/table/
  */
 
-import { forwardRef, ReactNode, Ref } from 'react';
+import { CSSProperties, forwardRef, ReactNode, Ref } from 'react';
 import { TableTbody } from './TableTbody';
 import { TableTd } from './TableTd';
 import { TableTh } from './TableTh';
@@ -13,13 +13,26 @@ export type TableRef = HTMLTableElement;
 
 export interface TableProps {
   children?: Array<React.ReactElement<HTMLTableSectionElement>> | any;
+  maxHeight?: string;
 }
 
 const Root = forwardRef(
-  ({ children }: { children: ReactNode }, ref: Ref<TableRef>) => {
+  (
+    { children, maxHeight }: { children: ReactNode; maxHeight?: string },
+    ref: Ref<TableRef>,
+  ) => {
+    const style: CSSProperties = {
+      maxHeight,
+      overflowY: 'auto',
+    };
+
     return (
-      <div className="table-responsive">
-        <table ref={ref} className="table align-middle mb-0">
+      <div className="table-responsive" style={maxHeight ? style : {}}>
+        <table
+          ref={ref}
+          className="table align-middle mb-0"
+          style={{ overflow: maxHeight ? 'visible' : 'hidden' }}
+        >
           {children}
         </table>
       </div>
