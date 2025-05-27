27044686c7b0c067a942b0bf1b44b91f093a75ef
Damien Romito Castro <damien.romito-castro@edifice.io>
2025-05-27 17:26:55 +0200

    feat(react)-Input-showcounter (#373)

--- packages/react/src/components/Input/Input.tsx
+++ packages/react/src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, Ref } from 'react';
+import { forwardRef, Ref, useState } from 'react';
 
 import clsx from 'clsx';
 
@@ -38,6 +38,10 @@ export interface InputProps
    * Optional class for styling purpose
    */
   className?: string;
+  /**
+   * Show count of characters
+   */
+  showCounter?: boolean;
 }
 
 /**
@@ -52,11 +56,15 @@ const Input = forwardRef(
       size = 'md',
       type = 'text',
       className,
+      showCounter,
       ...restProps
     }: InputProps,
     ref: Ref<HTMLInputElement>,
   ) => {
     const { id, isRequired, isReadOnly, status } = useFormControl();
+    const [currentLength, setCurrentLength] = useState(
+      restProps.value?.toString().length || 0,
+    );
 
     const classes = clsx(
       {
@@ -67,21 +75,40 @@ const Input = forwardRef(
         'is-valid': status === 'valid',
         'form-control-plaintext': isReadOnly,
         'no-validation-icon': noValidationIcon,
+        'pe-64': showCounter && !status,
       },
       className,
     );
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCurrentLength(e.target.value.length);
+      restProps.onChange?.(e);
+    };
+
     return (
-      <input
-        ref={ref}
-        id={id}
-        className={classes}
-        type={type}
-        placeholder={placeholder}
-        required={isRequired}
-        readOnly={isReadOnly}
-        {...restProps}
-      />
+      <div>
+        <input
+          ref={ref}
+          id={id}
+          className={classes}
+          type={type}
+          placeholder={placeholder}
+          required={isRequired}
+          readOnly={isReadOnly}
+          {...restProps}
+          onChange={handleChange}
+        />
+        {showCounter && !status && (
+          <span
+            className={clsx('caption text-end float-end mt-n32 py-2 px-12 ', {
+              'text-danger': currentLength === restProps.maxLength,
+              'text-gray-700': currentLength !== restProps.maxLength,
+            })}
+          >
+            {currentLength} / {restProps.maxLength}
+          </span>
+        )}
+      </div>
     );
   },
 );
