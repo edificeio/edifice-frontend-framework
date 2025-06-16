2b856f8eda1f63e461398fc16858012402800051
Headan <lucien.devarsdumayne@opendigitaleducation.com>
2025-06-16 16:45:38 +0200

    fix: #PEDAGO-2774, fix key console error

--- packages/react/src/modules/editor/components/Toolbar/TableToolbar.AddMenu.tsx
+++ packages/react/src/modules/editor/components/Toolbar/TableToolbar.AddMenu.tsx
@@ -69,12 +69,8 @@ export const TableToolbarAddMenu = ({ editor }: Props) => {
       </Tooltip>
       <Dropdown.Menu>
         {addOptions.map((option) => (
-          <div onMouseDown={(e) => e.preventDefault()}>
-            <Dropdown.Item
-              key={option.key}
-              icon={option.icon}
-              onClick={option.onClick}
-            >
+          <div key={option.key} onMouseDown={(e) => e.preventDefault()}>
+            <Dropdown.Item icon={option.icon} onClick={option.onClick}>
               {option.label}
             </Dropdown.Item>
           </div>
