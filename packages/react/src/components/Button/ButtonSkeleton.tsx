c340bb9b62a347364fb35da1a5d06aaf779d3cc2
RomuC <romuald.coulaud@gmail.com>
2025-06-16 10:45:36 +0200

    feat: #ENABLING-324 add editor skeleton + button skeleton

---
+++ packages/react/src/components/Button/ButtonSkeleton.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@edifice.io/react';
+import { ButtonColors, ButtonSizes } from './Button';
+
+export interface ButtonSkeletonProps {
+  className?: string;
+  color?: ButtonColors;
+  size?: ButtonSizes;
+}
+
+const ButtonSkeleton = ({
+  className,
+  color = 'tertiary',
+  size = 'md',
+}: ButtonSkeletonProps) => {
+  return (
+    <Button
+      className={`placeholder flex-shrink-1 ${className}`}
+      color={color}
+      variant="filled"
+      size={size}
+      disabled
+    ></Button>
+  );
+};
+
+ButtonSkeleton.displayName = 'ButtonSkeleton';
+
+export default ButtonSkeleton;
--- packages/react/src/components/Button/index.ts
+++ packages/react/src/components/Button/index.ts
@@ -1,7 +1,9 @@
 export { default as Button } from './Button';
+export { default as ButtonSkeleton } from './ButtonSkeleton';
 export { default as IconButton } from './IconButton';
 export { default as SearchButton } from './SearchButton';
 
 export * from './Button';
+export * from './ButtonSkeleton';
 export * from './IconButton';
 export * from './SearchButton';
