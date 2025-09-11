50dd398cc8abc5ae0effb6accd2e3e1fc200e0ca
RomuC <romuald.coulaud@gmail.com>
2025-09-11 10:12:02 +0200

    feat: #COCO-4357 add editor preview component

---
+++ packages/react/src/modules/editor/components/Editor/EditorPreview.tsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from 'react';
+
+import clsx from 'clsx';
+
+import { getThumbnail } from '@edifice.io/utilities';
+import { useTranslation } from 'react-i18next';
+import { Image } from '../../../../components';
+
+/**
+ * Editor component properties
+ */
+export interface EditorPreviewProps {
+  /**
+   * (Optional) id of the HTML element.
+   * Useful with an external `<FormControl id=...><Label/>`.
+   */
+  id?: string;
+  /** Rich content to render. */
+  content: string;
+  /** Display with or without a border */
+  variant?: 'outline' | 'ghost';
+  handleClickOnDetail?: () => void;
+  handleClickOnMedia?: () => void;
+  maxMediaDisplayed?: number;
+}
+
+const EditorPreview = ({
+  content,
+  variant = 'outline',
+  handleClickOnDetail,
+  handleClickOnMedia,
+  maxMediaDisplayed = 3,
+}: EditorPreviewProps) => {
+  const { t } = useTranslation();
+  const [summaryContent, setSummaryContent] = useState<string>('');
+  const [mediaURLs, setMediaURLs] = useState<string[]>([]);
+
+  const borderClass = clsx(variant === 'outline' && 'border rounded-3');
+  const contentClass = clsx(
+    'flex-fill text-truncate text-truncate-2 post-preview-content mt-16 overflow-hidden',
+    variant === 'outline' && 'my-12 mx-16',
+  );
+
+  const stripHtml = (html: string) => {
+    // Create a temporary element
+    const tmp = document.createElement('div');
+    tmp.innerHTML = html;
+    // Get text content only
+    return tmp.textContent || tmp.innerText || '';
+  };
+
+  useEffect(() => {
+    let contentHTML = content;
+    if (contentHTML) {
+      const getMediaTags = /<(img|video|iframe|audio|embed)[^>]*>(<\/\1>)?/gim;
+      const getSrc = /src=(?:"|')([^"|']*)(?:"|')/;
+      const mediaTags = contentHTML.match(getMediaTags);
+      contentHTML = contentHTML.replace(getMediaTags, '');
+      if (mediaTags?.length) {
+        setMediaURLs(
+          mediaTags
+            .filter((tag) => tag.includes('img'))
+            .map((tag) => {
+              const srcMatch = getSrc.exec(tag);
+              if (srcMatch?.length) {
+                return getThumbnail(srcMatch[1], 0, 300);
+              }
+              return '';
+            }) || [],
+        );
+      }
+
+      setSummaryContent(stripHtml(contentHTML));
+    }
+  }, [content]);
+
+  return (
+    <div className={borderClass} data-testid="editor-preview">
+      <div
+        onClick={handleClickOnDetail}
+        tabIndex={-1}
+        role="button"
+        className={contentClass}
+      >
+        {summaryContent}
+      </div>
+      <div
+        onClick={handleClickOnMedia}
+        tabIndex={-1}
+        role="button"
+        className="d-flex align-items-center justify-content-center gap-24 px-32 pt-16"
+      >
+        {mediaURLs.slice(0, maxMediaDisplayed).map((url, index) => (
+          <div
+            className={clsx('position-relative col-12 col-md-4 ', {
+              'd-none d-md-block': index >= 1,
+            })}
+            key={url}
+          >
+            <Image
+              alt=""
+              objectFit="cover"
+              ratio="16"
+              className="rounded"
+              src={url}
+              sizes=""
+            />
+            {(index === 0 || index === 2) &&
+              mediaURLs.length - (index + 1) > 0 && (
+                <div
+                  className={clsx(
+                    'position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center rounded text-light bg-dark bg-opacity-50',
+                    {
+                      'd-flex d-md-none': index === 0,
+                      'd-none d-md-flex': index === 2,
+                    },
+                  )}
+                >
+                  {t('editor.preview.moreMedia', {
+                    mediaCount: mediaURLs.length - (index + 1),
+                  })}
+                </div>
+              )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EditorPreview;
--- packages/react/src/modules/editor/components/Editor/index.ts
+++ packages/react/src/modules/editor/components/Editor/index.ts
@@ -1,3 +1,4 @@
 export * from './Editor';
 export { default as Editor } from './Editor';
+export { default as EditorPreview } from './EditorPreview';
 export { default as EditorSkeleton } from './EditorSkeleton';
