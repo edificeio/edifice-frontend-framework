66f7e642a1dbd669025151d7dd6cad8355d90dcf
RomuC <romuald.coulaud@gmail.com>
2025-09-15 13:50:25 +0200

    Add divier component

---
+++ packages/react/src/components/Divider/Divider.tsx
@@ -0,0 +1,26 @@
+import { Flex } from '@edifice.io/react';
+import { ReactNode } from 'react';
+
+export function Divider({
+  children,
+  color = 'var(--edifice-gray-400)',
+}: {
+  children: ReactNode[];
+  color: string;
+}) {
+  return (
+    <Flex align="center" justify="around" gap="16" wrap="nowrap">
+      <hr
+        className="divider m-12 ms-0 flex-fill"
+        style={{ borderColor: color }}
+      />
+      <Flex gap="12" align="center" justify="around">
+        {...children}
+      </Flex>
+      <hr
+        className="divider m-12 me-0 flex-fill"
+        style={{ borderColor: color }}
+      />
+    </Flex>
+  );
+}
