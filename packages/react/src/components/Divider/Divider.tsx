39daca4e1f89553fef1f46e39f51c0622a5e0597
RomuC <romuald.coulaud@gmail.com>
2025-09-16 15:33:36 +0200

    feat: #COCO-4595 addSeparatedInfo, Divider components (#406)

---
+++ packages/react/src/components/Divider/Divider.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+import { Flex } from '../Flex';
+
+export function Divider({
+  children,
+  color = 'var(--edifice-gray-400)',
+}: {
+  children: ReactNode[];
+  color: string;
+}) {
+  return (
+    <Flex
+      align="center"
+      justify="around"
+      gap="16"
+      wrap="nowrap"
+      className="flex-fill"
+    >
+      <hr
+        className="divider m-12 ms-0 flex-fill"
+        style={{ borderColor: color }}
+      />
+      <Flex gap="12" align="center" justify="around">
+        {...children}
+      </Flex>
+      <hr
+        className="divider m-12 me-0 flex-fill"
+        style={{ borderColor: color }}
+      />
+    </Flex>
+  );
+}
