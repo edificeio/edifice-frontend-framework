cee353ceacb92ca7ba9e24fc4a5dc1eb7da1a4b6
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2025-10-14 14:05:28 +0200

    fix: #PEDAGO-3218, should not use BroadcastChanel which aim is cross-tab communication

--- packages/client/src/ts/notify/Subject.ts
+++ packages/client/src/ts/notify/Subject.ts
@@ -4,43 +4,77 @@ import {
   ISubscription,
   LayerName,
 } from './interfaces';
-import { Observable } from './Observable';
+import { SimpleChannel } from './SimpleChannel';
+
+type RevokationFunction = () => void;
+
+class Subscription<T extends ISubjectMessage> implements ISubscription {
+  public revoke: RevokationFunction;
+
+  constructor(
+    private _channel?: SimpleChannel<T>,
+    handler?: (message: T) => void,
+  ) {
+    this.revoke = this.setReceiver((message: T) => handler?.(message));
+  }
+
+  private setReceiver(receiver: (message: T) => void): RevokationFunction {
+    if (!this._channel) return () => {};
+    const unsubscribe = this._channel.listen(receiver);
+    return () => {
+      unsubscribe();
+      // do not close the shared channel on single unsubscribe (keeps it reusable)
+      this._channel = undefined;
+    };
+  }
+}
 
 //-------------------------------------
 // Event system
 //-------------------------------------
 export class Subject<T extends ISubjectMessage> implements ISubject<T> {
-  private observables = new Map<string, Observable<T>>();
+  /* We maintain channels for sending messages.
+   * Receiving channels are instantiated per subscription.
+   */
+  private publishChannels: Map<string, SimpleChannel<T>> = new Map<
+    string,
+    SimpleChannel<T>
+  >();
 
-  private getSubjectName(layer: string): string {
+  private getChannelName(layer: string): string {
     return 'Subject:' + layer;
   }
 
-  private getObservable(layer: string): Observable<T> {
-    const name = this.getSubjectName(layer);
-    let observable = this.observables.get(name);
-    if (!observable) {
-      observable = new Observable();
-      this.observables.set(name, observable);
+  private getPublishChannel(layer: string): SimpleChannel<T> {
+    const name = this.getChannelName(layer);
+    let channel = this.publishChannels.get(name);
+    if (!channel) {
+      channel = this.newChannel(layer);
+      this.publishChannels.set(name, channel);
     }
-    return observable;
+    return channel;
+  }
+
+  public newChannel(layer: string): SimpleChannel<any> {
+    const name = this.getChannelName(layer);
+    return SimpleChannel.getChannel(name);
   }
 
   publish(layer: LayerName, message: T): void {
-    typeof layer === 'string' && this.getObservable(layer).postMessage(message);
-  }
-
-  subscribe(layer: LayerName, handler: (message: T) => void): ISubscription {
     if (typeof layer === 'string') {
-      const observable = this.getObservable(layer);
-      return observable.addEventListener(handler);
-    } else {
-      return { revoke: () => {} } as ISubscription;
+      this.getPublishChannel(layer).publish(message);
     }
   }
 
-  public getSubscriberCountFor(layer: LayerName) {
-    const observable = this.getObservable(layer);
-    return observable.subscriberCount;
+  subscribe<T extends ISubjectMessage>(
+    layer: LayerName,
+    handler: (message: T) => void,
+  ): ISubscription {
+    if (typeof layer === 'string') {
+      const receiveChannel = this.newChannel(layer) as SimpleChannel<T>;
+      return new Subscription(receiveChannel, handler);
+    } else {
+      return new Subscription();
+    }
   }
 }
