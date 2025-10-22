d83cdde417f40bcfcb8441851deedf0117d3ad1a
RomuC <romuald.coulaud@gmail.com>
2025-10-22 11:09:35 +0200

    fix(react): #COCO-4509 select set default value (#414)

--- packages/react/src/components/Select/Select.tsx
+++ packages/react/src/components/Select/Select.tsx
@@ -23,7 +23,7 @@ export interface OptionsType {
 
 export interface SelectProps
   extends Omit<DropdownProps, 'children'>,
-    Omit<DropdownTriggerProps, 'badgeContent'> {
+    Omit<DropdownTriggerProps, 'badgeContent' | 'defaultValue'> {
   /**
    * Default select label
    */
@@ -36,6 +36,10 @@ export interface SelectProps
    * Callback to get value
    */
   onValueChange?: (option: OptionsType | string) => void;
+  /**
+   * Default value
+   */
+  defaultValue?: string;
 }
 
 /**
@@ -53,6 +57,7 @@ const Select = ({
   disabled,
   placeholderOption,
   onValueChange,
+  defaultValue,
 }: SelectProps) => {
   const [localValue, setLocalValue] = useState<OptionsType | string>();
 
@@ -66,6 +71,19 @@ const Select = ({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [localValue]);
+
+  useEffect(() => {
+    if (defaultValue) {
+      const foundOption = options.find((option) => {
+        const value = typeof option === 'object' ? option.value : option;
+        return value === defaultValue;
+      });
+      if (foundOption !== undefined) {
+        setLocalValue(foundOption);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const label = typeof localValue === 'object' ? localValue.label : localValue;
   const iconChange =
