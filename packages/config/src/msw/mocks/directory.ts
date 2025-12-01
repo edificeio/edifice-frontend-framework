8310373757f6ec4973eb8a5b6c0950ca6237f35a
RomuC <romuald.coulaud@gmail.com>
2025-12-01 11:50:49 +0100

    feat(react, client): #COCO-4511 udpate share rigths component to be able to use it for other than explorer ressource (#424)

--- packages/config/src/msw/mocks/directory.ts
+++ packages/config/src/msw/mocks/directory.ts
@@ -16,4 +16,34 @@ export const handlers = [
       hobbies: [],
     });
   }),
+  http.get('/directory/sharebookmark/all', () => {
+    return HttpResponse.json([
+      {
+        id: '_9a1d29c3d2864ed8a3d72198fadf4a96',
+        name: 'Parents délégués CE2',
+      },
+    ]);
+  }),
+  http.get('/directory/sharebookmark/_9a1d29c3d2864ed8a3d72198fadf4a96', () => {
+    return HttpResponse.json({
+      id: '_9a1d29c3d2864ed8a3d72198fadf4a96',
+      name: 'Parents délégués CE2',
+      notVisibleCount: 0,
+      groups: [],
+      users: [
+        {
+          displayName: 'CARPENTIER Béatrice',
+          profile: 'Relative',
+          id: 'c0824335-ab0e-41fb-9ed3-d5c28a93087d',
+          activationCode: false,
+        },
+        {
+          displayName: 'ROUSTIN Christophe',
+          profile: 'Relative',
+          id: '6a7495f8-bec7-4f68-b891-0b05c6e8e0ce',
+          activationCode: false,
+        },
+      ],
+    });
+  }),
 ];
---
+++ packages/react/src/modules/modals/ShareModal/ShareRessources.stories.tsx
@@ -0,0 +1,71 @@
+import { ShareRightActionDisplayName } from '@edifice.io/client';
+import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import ShareResources, {
+  ShareOptions,
+  ShareResourcesRef,
+} from './ShareResources';
+
+// Mock data for props
+const mockShareOptions: ShareOptions = {
+  resourceId: 'resource-1',
+  resourceRights: [],
+  resourceCreatorId: 'user-1',
+};
+
+const meta: Meta<typeof ShareResources> = {
+  title: 'Modules/ShareResources',
+  component: ShareResources,
+  globals: {
+    app: 'actualites',
+  },
+  args: {
+    shareOptions: mockShareOptions,
+    onSuccess: () => alert('Shared!'),
+    onChange: (rights, isDirty) => {
+      console.log('Rights changed:', rights, 'Is dirty:', isDirty);
+    },
+    onSubmit: () => {
+      console.log('Is submitting');
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ShareResources>;
+
+export const Default: Story = {
+  render: (args, { ref }) => (
+    <ShareResources {...args} ref={ref as React.Ref<ShareResourcesRef>} />
+  ),
+};
+
+export const OverrideUrls: Story = {
+  args: {
+    shareOptions: {
+      ...mockShareOptions,
+      shareUrls: {
+        getResourceRights: '/actualites/api/v1/infos/resource-1/shares',
+        saveResourceRights: '/actualites/api/v1/infos/resource-1/shares',
+        getShareMapping: '/actualites/api/v1/rights/sharing',
+      },
+      filteredActions: ['read', 'comment'] as ShareRightActionDisplayName[],
+    },
+  },
+  render: (args, { ref }) => (
+    <ShareResources {...args} ref={ref as React.Ref<ShareResourcesRef>} />
+  ),
+};
+
+export const FilterActions: Story = {
+  args: {
+    shareOptions: {
+      ...mockShareOptions,
+      filteredActions: ['read', 'comment'] as ShareRightActionDisplayName[],
+    },
+  },
+  render: (args, { ref }) => (
+    <ShareResources {...args} ref={ref as React.Ref<ShareResourcesRef>} />
+  ),
+};
