25d7000eeee01e3772dd5d6bc0e9d1e9765af9dd
RomuC <romuald.coulaud@gmail.com>
2025-12-08 16:15:00 +0100

    update to gray 500

--- packages/react/src/components/Divider/Divider.tsx
+++ packages/react/src/components/Divider/Divider.tsx
@@ -7,7 +7,8 @@ import { ReactNode } from 'react';
  * @param props - The component props
  * @param props.children - Content to be displayed inside the divider
  * @param props.vertical - Whether the divider is vertical. Defaults to false
- * @param props.className - Optional CSS class name for additional styling
+ * @param props.className - Optional CSS class name for additional styling of the divider
+ * @default 'border-gray-500'
  * @param props.style - Optional inline styles for the divider
  *
  * @returns A React component that renders a divider with the specified properties
@@ -30,7 +31,7 @@ import { ReactNode } from 'react';
 const Divider = ({
   children,
   vertical = false,
-  className = 'border-gray-400',
+  className = 'border-gray-500',
   style,
 }: {
   children?: ReactNode;
