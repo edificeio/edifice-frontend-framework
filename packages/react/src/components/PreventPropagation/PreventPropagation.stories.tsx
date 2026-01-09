9a6e6c1c532feb11adc8e1b27249bfa0f7d0cab4
Pascal Saussier <pascal.saussier@edifice.io>
2026-01-09 17:32:51 +0100

    refactor(storybook) update storybook to v9

--- packages/react/src/components/PreventPropagation/PreventPropagation.stories.tsx
+++ packages/react/src/components/PreventPropagation/PreventPropagation.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-vite';
 import PreventPropagation from './PreventPropagation';
 import { Button } from '../Button';
 import { Switch } from '../Switch';
--- packages/react/src/hooks/useCheckable/useCheckable.stories.tsx
+++ packages/react/src/hooks/useCheckable/useCheckable.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-vite';
 import { Checkbox, Table } from '../..';
 import { useCheckable } from './useCheckable';
 
