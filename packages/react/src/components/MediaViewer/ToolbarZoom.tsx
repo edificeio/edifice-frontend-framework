ae23d6123e177411352e6c19feec4b9af378e735
Headan <36904370+Headan@users.noreply.github.com>
2026-01-15 11:50:13 +0100

    fix: #PEDAGO-3462, create MediaViewer component (#435)

---
+++ packages/react/src/components/MediaViewer/ToolbarZoom.tsx
@@ -0,0 +1,29 @@
+import { Flex, IconButton } from '@edifice.io/react';
+import { IconMinus, IconPlus, IconZoomIn } from '@edifice.io/react/icons';
+
+export default function ToolbarZoom({
+  zoomIn,
+  zoomOut,
+}: {
+  zoomIn: () => void;
+  zoomOut: () => void;
+}) {
+  return (
+    <Flex justify="center" className="media-viewer-toolbar-zoom-container">
+      <Flex gap="4" className="p-12 media-viewer-toolbar-zoom" align="center">
+        <IconButton
+          variant="ghost"
+          icon={<IconMinus color="#fff" />}
+          onClick={zoomOut}
+        />
+
+        <IconZoomIn color="#fff" className="m-4" />
+        <IconButton
+          variant="ghost"
+          icon={<IconPlus color="#fff" />}
+          onClick={zoomIn}
+        />
+      </Flex>
+    </Flex>
+  );
+}
