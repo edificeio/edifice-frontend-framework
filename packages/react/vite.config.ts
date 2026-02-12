6dcad786a38e6b39b4d1d74d3adaddd3414c74eb
Pascal Saussier <pascal.saussier@edifice.io>
2026-02-12 12:04:08 +0100

    chore: fix dependencies issues, lint

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,97 +1,105 @@
-/// <reference types="vitest/config" />
-
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { PluginPure } from 'rollup-plugin-pure';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig, PluginOption } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 import { removeDsn } from '../../plugins/remove-display-name';
 import { dependencies, peerDependencies } from './package.json';
 
-export default defineConfig({
-  esbuild: {
-    minifyIdentifiers: false,
-  },
+export default defineConfig(({ mode }) => {
+  const isTest = mode === 'test';
+  const isAnalyze = mode === 'analyze';
 
-  build: {
-    lib: {
-      entry: {
-        'audience': resolve(__dirname, 'src/modules/audience/index.ts'),
-        'comments': resolve(__dirname, 'src/modules/comments/index.ts'),
-        'editor': resolve(__dirname, 'src/modules/editor/index.ts'),
-        'icons': resolve(__dirname, 'src/modules/icons/components/index.ts'),
-        'icons-nav': resolve(
-          __dirname,
-          'src/modules/icons/components/nav/index.ts',
-        ),
-        'icons-apps': resolve(
-          __dirname,
-          'src/modules/icons/components/apps/index.ts',
-        ),
-        'icons-audience': resolve(
-          __dirname,
-          'src/modules/icons/components/audience/index.ts',
-        ),
-        'modals': resolve(__dirname, 'src/modules/modals/index.ts'),
-        'multimedia': resolve(__dirname, 'src/modules/multimedia/index.ts'),
-        'index': resolve(__dirname, 'src/index.ts'),
-      },
-      formats: ['es'],
+  return {
+    esbuild: {
+      minifyIdentifiers: false,
     },
-    rollupOptions: {
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
-      external: [
-        ...Object.keys(dependencies),
-        ...Object.keys(peerDependencies),
-        'react/jsx-runtime',
-        '@edifice.io/client',
-        /^@edifice\.io\/tiptap-extensions\/.*/,
-        /^@edifice\.io\/bootstrap\/.*/,
-        /^dayjs\/plugin\/.+\.js$/,
-        /^dayjs\/locale\/.+\.js$/,
-        /^swiper\/.*/,
-        /^@edifice-ui\/icons\/.*/,
-      ],
-    },
-  },
 
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@babel/plugin-transform-react-pure-annotations'],
+    plugins: [
+      react({
+        babel: {
+          plugins: ['@babel/plugin-transform-react-pure-annotations'],
+        },
+      }),
+
+      tsconfigPaths(),
+
+      // Pas utile en test
+      !isTest &&
+        removeDsn({
+          includeExtensions: ['.ts', '.tsx'],
+          excludeExtensions: ['.stories.tsx'],
+        }),
+
+      !isTest &&
+        dts({
+          tsconfigPath: './tsconfig.build.json',
+        }),
+
+      !isTest &&
+        PluginPure({
+          functions: ['Object.assign'],
+        }),
+
+      isAnalyze && visualizer(),
+    ].filter(Boolean),
+
+    build: {
+      lib: {
+        entry: {
+          'audience': resolve(__dirname, 'src/modules/audience/index.ts'),
+          'comments': resolve(__dirname, 'src/modules/comments/index.ts'),
+          'editor': resolve(__dirname, 'src/modules/editor/index.ts'),
+          'icons': resolve(__dirname, 'src/modules/icons/components/index.ts'),
+          'icons-nav': resolve(
+            __dirname,
+            'src/modules/icons/components/nav/index.ts',
+          ),
+          'icons-apps': resolve(
+            __dirname,
+            'src/modules/icons/components/apps/index.ts',
+          ),
+          'icons-audience': resolve(
+            __dirname,
+            'src/modules/icons/components/audience/index.ts',
+          ),
+          'modals': resolve(__dirname, 'src/modules/modals/index.ts'),
+          'multimedia': resolve(__dirname, 'src/modules/multimedia/index.ts'),
+          'index': resolve(__dirname, 'src/index.ts'),
+        },
+        formats: ['es'],
       },
-    }),
-    removeDsn({
-      includeExtensions: ['.ts', '.tsx'],
-      excludeExtensions: ['.stories.tsx'],
-    }),
-    tsconfigPaths(),
-    dts({
-      tsconfigPath: './tsconfig.build.json',
-      compilerOptions: {
-        baseUrl: '.',
-        paths: {
-          '@tanstack/react-query': ['node_modules/@tanstack/react-query'],
+
+      rollupOptions: {
+        external: [
+          ...Object.keys(dependencies ?? {}),
+          ...Object.keys(peerDependencies ?? {}),
+          'react/jsx-runtime',
+          '@edifice.io/client',
+          /^@edifice\.io\/tiptap-extensions\/.*/,
+          /^@edifice\.io\/bootstrap\/.*/,
+          /^dayjs\/plugin\/.+\.js$/,
+          /^dayjs\/locale\/.+\.js$/,
+          /^swiper\/.*/,
+          /^@edifice-ui\/icons\/.*/,
+        ],
+        output: {
+          preserveModules: true,
+          preserveModulesRoot: 'src',
         },
       },
-    }),
-    PluginPure({
-      functions: ['Object.assign'],
-    }) as PluginOption,
-    visualizer() as PluginOption,
-  ],
+    },
 
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.spec.tsx'],
-    setupFiles: ['./vitest.setup.ts'],
-    reporters: ['default'],
-  },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      include: ['src/**/*.spec.tsx'],
+      setupFiles: ['./vitest.setup.ts'],
+      watch: false,
+      clearMocks: true,
+      restoreMocks: true,
+    },
+  };
 });
