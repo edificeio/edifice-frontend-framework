5e27900a143843e7f8ba33f1d4c13d681667113f
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-03-23 16:57:36 +0100

    add widgets preferences support

--- packages/react/src/modules/homepage/components/SchoolWidget/useUserSchools.ts
+++ packages/react/src/modules/homepage/components/SchoolWidget/useUserSchools.ts
@@ -1,9 +1,12 @@
-import { School } from '@edifice.io/client';
+import { School, WIDGET_NAME, WidgetUserPref } from '@edifice.io/client';
 import { useEffect, useState } from 'react';
 import { useEdificeClient } from 'src/providers';
+import useWidgetPreferences from '../../hooks/useWidgetPreferences';
 
 export function useUserSchools() {
   const { userDescription } = useEdificeClient();
+  const { lookup, saveUserPreferences } = useWidgetPreferences();
+  const [userPreferences, setUserPreferences] = useState<WidgetUserPref>();
   const [selectedSchool, setSelectedSchool] = useState<School>();
 
   const schools = userDescription?.schools;
@@ -12,11 +15,33 @@ export function useUserSchools() {
     setSelectedSchool(schools?.[0]);
   }, []);
 
+  useEffect(() => {
+    const userPref = lookup?.(WIDGET_NAME.SCHOOL)?.userPref;
+    setUserPreferences(userPref);
+  }, [lookup]);
+
+  useEffect(() => {
+    if (userPreferences?.schoolId && Array.isArray(schools)) {
+      const index = schools.findIndex(
+        (school) => school.id === userPreferences?.schoolId,
+      );
+      setSelectedSchool(
+        index < 0 || index >= schools.length ? schools[0] : schools[index],
+      );
+    } else {
+      setSelectedSchool(undefined);
+    }
+  }, [userPreferences]);
+
   return {
     schools: schools ?? [],
     selectedSchool,
     handleSelectedSchoolChange: (school: School) => {
       setSelectedSchool(school);
+      if (userPreferences) {
+        userPreferences.schoolId = school.id;
+        saveUserPreferences();
+      }
     },
   };
 }
