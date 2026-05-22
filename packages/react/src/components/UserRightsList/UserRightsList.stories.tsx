1434017d88c3ee40de898e1136c09996cb464033
Pascal Saussier <pascal.saussier@edifice.io>
2026-05-22 17:11:30 +0200

    feat: ENABLING-821 add HomeCard

--- packages/react/src/components/UserRightsList/UserRightsList.stories.tsx
+++ packages/react/src/components/UserRightsList/UserRightsList.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react-vite';
 import { fn } from 'storybook/test';
 import type { ComponentRef } from 'react';
 import { useEffect, useRef } from 'react';
