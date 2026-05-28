08a18971b0cf5a4377f2b61b8b311c9b5827c785
RomuC <romuald.coulaud@gmail.com>
2026-05-28 15:28:58 +0200

    remove warning

--- packages/react/src/modules/homepage/components/Notifications/NotificationSkeleton.tsx
+++ packages/react/src/modules/homepage/components/Notifications/NotificationSkeleton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotificationSkeleton = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->((props, ref) => <div className="placeholder py-24 mb-16" ref={ref}></div>);
+>((_props, ref) => <div className="placeholder py-24 mb-16" ref={ref}></div>);
 
 NotificationSkeleton.displayName = 'NotificationSkeleton';
 
