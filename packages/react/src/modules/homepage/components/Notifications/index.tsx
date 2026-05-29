bff90ad3729730eb8a98bb6a9a252d19caaea04b
RomuC <romuald.coulaud@edifice.io>
2026-05-29 11:29:48 +0200

    feat(react): #COCO-5664 add notification container (#487)

---
+++ packages/react/src/modules/homepage/components/Notifications/index.tsx
@@ -0,0 +1,5 @@
+export * from './Notification';
+export * from './NotificationList';
+export * from './NotificationListContainer';
+
+export { default as NotificationList } from './NotificationList';
